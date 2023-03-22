d7a43265b5eef602486f0a9e1bc3198138e8a7c5
Michal Brath <michal.brath@iblsoft.com>
2023-03-22 12:56:33 +0100

    Polish circular buffer, add weather stats page

--- src/weather_station_prototype.cpp
+++ src/weather_station_prototype.cpp
@@ -6,76 +6,37 @@
 #include "DHT.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "pico/stdlib.h"
-
-#include <hardware/flash.h>
-uint32_t ints = save_and_disable_interrupts();
-
-char *p = (char *)XIP_BASE;
-char *first_byte = *p;
-flash_range_erase (first_byte, 10);
-flash_range_program (first_byte, buffer, 10);
-restore_interrupts (ints);
+#define MEASURE_BUFFER_SIZE 50
 
 
-const char ssid[] = "XXXXXXXXXXXXX";
-const char password[]= "XXXXXXXXXXXXX";
+static int milisecondsFromLastMeasurement = 999999;
+#define SAMPLING_PERIOD_SEC 60*15
+int getSecondsFromLastMeasurement(){return milisecondsFromLastMeasurement/1000; };
+
+const char ssid[] = "xxxxxxxxxxxxxxxxx";
+const char password[]= "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
 WebServer server(80);
 
-/////////////// MEASURED VALUES STORAGE ////////
-#define MEASURE_BUFFER_SIZE 50
-float valuesTemperature[MEASURE_BUFFER_SIZE];
-int valuesTemperaturePtr = 0;
-float valuesHumidity[MEASURE_BUFFER_SIZE];
-int valuesHumidityPtr = 0;
-float valuesPressure[MEASURE_BUFFER_SIZE];
-int valuesPressurePtr = 0;
-
-void initBuffers(){
-  for(int i = 0; i < MEASURE_BUFFER_SIZE; i++){
-    valuesPressure[i] = 0;
-    valuesHumidity[i] = 0;
-    valuesTemperature[i] = 0;
-  }
-}
-
-void  bufferWrite(float storage[MEASURE_BUFFER_SIZE], int &ptr, float value){
-  storage[ptr % MEASURE_BUFFER_SIZE] = value;
-  ptr++; 
-}
-
-void  bufferToJson(float storage[MEASURE_BUFFER_SIZE], int &ptr, char* outBuffer, int bufferSize){
-  int stringWritePtr=1;
-  outBuffer[0]='[';
-  for(int i = 0; i < MEASURE_BUFFER_SIZE; i++){
-    // iterate back
-    int idx = (ptr - 1 - i) %MEASURE_BUFFER_SIZE;
-    stringWritePtr += sprintf(outBuffer + stringWritePtr, "%.1f,", storage[idx]);
-  }
-  outBuffer[stringWritePtr - 1] = ']';
-  outBuffer[stringWritePtr] = '\0';
-}
-
-//////////////// DHT GLOBAL ////////////////////
+//////////////// TEMP HUMIDITY SENSOR GLOBAL ////
 #define DHTPIN 13
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-//////////////// BMP GLOBAL ////////////////////
+//////////////// PRESSURE SENSOR GLOBAL /////////
 #undef BMP280_ADDRESS
 #define BMP280_ADDRESS 0x76
 Adafruit_BMP280 bmp; // I2C
 
-//////////////// LCD GLOBAL ////////////////////
+//////////////// DISPLAY GLOBAL ////////////////////
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define SCREEN_NUMBER_OF_LINES 3
+#define SCREEN_NUMBER_OF_CHARS 50
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C // See datasheet for Address
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-//////////////// LCD ////////////////////
+//////////////// DISPLAY ////////////////////
 void initLcd(){
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -88,10 +49,12 @@ void initLcd(){
   display.display();
 }
 
-void drawText(char text[SCREEN_NUMBER_OF_LINES][25]) {
+void drawText(char text[SCREEN_NUMBER_OF_LINES][SCREEN_NUMBER_OF_CHARS]) {
+  //adds diagonal movement to screen for longer lifetime of display
   static int lineOffset = 0;
   if(lineOffset++ == 2)
     lineOffset = 0;
+
   display.clearDisplay();
   display.setTextSize(1); // Draw 2X-scale text
   display.setTextColor(SSD1306_WHITE);
@@ -104,7 +67,7 @@ void drawText(char text[SCREEN_NUMBER_OF_LINES][25]) {
   delay(100);
 }
 
-//////////////// DHT ////////////////////
+//////////////// TEMPERATURE HUMIDITY SENSOR ////////////////////
 void initSensors(){
   // init dht
   dht.begin();
@@ -124,23 +87,20 @@ void initSensors(){
 
 float getTemperature(bool retFarenheit=false){
   float t = dht.readTemperature(retFarenheit);
-  Serial.println(t);
   return t;
 }
 
 float getHumidity(){
   float h = dht.readHumidity();
-  Serial.println(h);
   return h;
 }
 
 float getPressure(){
   float p = bmp.readPressure();
-  Serial.println(p);
   return p;
 }
 
-//////////////// WIFI ///////////////////
+//////////////// WIFI //////////////////////////////////
 void initWifi(){
   // Operate in WiFi Station mode
   WiFi.mode(WIFI_STA);
@@ -161,15 +121,84 @@ void initWifi(){
   Serial.println(WiFi.localIP());
 }
 
-//////////////// SERVER ///////////////////
+/////////////// MEASURED VALUES STORAGE ////////
+class MeasureBuffer
+{
+  float values[MEASURE_BUFFER_SIZE];
+  int ptr;
+public:
+  void clear(){
+    for(int i = 0; i < MEASURE_BUFFER_SIZE; i++){
+      values[i] = 0;
+    }
+  }
+
+  void  write(float value){
+    values[ptr] = value;
+    ptr = (ptr + 1) % MEASURE_BUFFER_SIZE; 
+  }
+
+  int getPrev(int ptr){
+    int idx = ptr - 1;
+    if (idx < 0)
+        idx = MEASURE_BUFFER_SIZE - 1;
+    return idx;
+  }
+
+  void  bufferToJson(char* outBuffer){
+    int stringWritePtr=1;
+    outBuffer[0]='[';
+    int idx = getPrev(ptr);
+    for(int i = 0; i < MEASURE_BUFFER_SIZE; i++){
+      stringWritePtr += sprintf(outBuffer + stringWritePtr, "%.1f, ", values[idx]);
+      idx = getPrev(idx);
+    }
+    outBuffer[stringWritePtr - 2] = ']';
+    outBuffer[stringWritePtr - 1] = '\0';
+  } 
+
+  void printVals(){
+    int idx = getPrev(ptr);
+    for(int i = 0; i < MEASURE_BUFFER_SIZE; i++){
+      Serial.print(values[idx]);
+      Serial.print(" ");
+      idx = getPrev(ptr);
+    }
+    Serial.println(" ");
+  }
+};
+
+MeasureBuffer valuesTemperature;
+MeasureBuffer valuesHumidity;
+MeasureBuffer valuesPressure;
+
+void arrayOfValuesToJsonResponseHelper(char valuesJson[MEASURE_BUFFER_SIZE * 50]){
+  char returnBuffer[MEASURE_BUFFER_SIZE * 100];
+  sprintf(
+    returnBuffer,
+    "{\"samplingPeriodSec\":%d, \"lastSampleSec\": %d, \"values\": %s}",
+    SAMPLING_PERIOD_SEC,
+    getSecondsFromLastMeasurement(),
+    valuesJson
+  );
+
+  server.send(200, "text/html", returnBuffer);
+}
+
+
+//////////////// SERVER ////////////////////////////////
 void initServer(){
   server.begin();
   server.on("/", handleMain);
   server.on("/temperature", handleTemperature);
   server.on("/humidity", handleHumidity);
   server.on("/pressure", handlePressure);
+  server.on("/temperaturearray", handleTemperatureArray);
+  server.on("/humidityarray", handleHumidityArray);
+  server.on("/pressurearray", handlePressureArray);
   server.onNotFound(handleNotFound);
-  // post request example returns what was passed
+
+  // post request example returns what was passed as page arg
   server.on(
     "/upload",
     HTTP_POST,
@@ -181,18 +210,15 @@ void initServer(){
       server.send(200, "text/plain", buffer); 
     }
   );
+
   server.begin();
   server.enableCORS(true);
   Serial.println("HTTP server started");
 }
 
 void handleMain(){
-  char html[] = "<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js\">\r\n<body style=\"padding-top:5px; background:#000\">\r\n<div class=\"container\">\r\n  \r\n </div>\r\n<!-- Optional JavaScript -->\r\n<!-- jQuery first, then Popper.js, then Bootstrap JS -->\r\n<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\r\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>\r\n<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script></script>\r\n<script>\r\n$(document).ready(function(){\r\n  $(\"button\").click(function(){\r\n\t\r\n\r\n      $.ajax({\r\n          url: \"http://10.72.69.101/temperature\",\r\n          type: \'GET\',\r\n          dataType: \'json\',\r\n          cors: true ,\r\n          contentType:\'application/json\',\r\n          secure: true,\r\n                    headers: {\r\n                        \'Access-Control-Allow-Origin\': \'*\',\r\n                    },\r\n          beforeSend: function (xhr) {\r\n              xhr.setRequestHeader (\"Authorization\", \"Basic \" + btoa(\"\"));\r\n          },\r\n          success: function (data){\r\n            console.log(data.organizations[0].name);\r\n            var organisation = data.organizations[0].name;\r\n            $(\"#company\").text(organisation);\r\n          }\r\n        })\r\n\r\n\r\n  });\r\n});\r\n\r\n\r\n\r\n</script>\r\n</head>\r\n<body>\r\n\r\n<button>Send an HTTP GET request to a page and get the result back</button>\r\n\r\n</body>\r\n</html>";
+  char html[] = "<!DOCTYPE html>\r\n<html>\r\n<head>\r\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n\t<style>\r\n\tbody {\r\n\t\tbackground: #CDD;\r\n\t}\r\n\r\n\t.content {\r\n\t\tmax-width: 500px;\r\n\t\tmargin: auto;\r\n\t\tbackground: white;\r\n\t\tpadding: 10px;\r\n\t}\r\n\t</style>\r\n\t<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js\"></script>\r\n\t<script src=\"https://code.jquery.com/jquery-3.1.1.min.js\"></script>\r\n\t<script>\r\n\tfunction getData(color, quantityName, path){\r\n\t    //const baseurl = \"http://10.72.69.101/\";\r\n\t    const baseurl = \"\";\r\n\r\n\t\t$.ajax({\r\n\t\t\turl: baseurl + path,\r\n\t\t\ttype: \'GET\',\r\n\t\t\tdataType: \'json\',\r\n\t\t\tcors: true ,\r\n\t\t\tcontentType:\'application/json\',\r\n\t\t\tsecure: true,\r\n\t\t\theaders: {\r\n\t\t\t\t\'Access-Control-Allow-Origin\': \'*\',\r\n\t\t\t},\r\n\t\t\tbeforeSend: function (xhr) {\r\n\t\t\t  xhr.setRequestHeader (\"Authorization\", \"Basic \" + btoa(\"\"));\r\n\t\t\t},\r\n\t\t\tsuccess: function (data){\r\n\t\t\t\tconsole.log(JSON.stringify(data));\r\n\t\t\t\tvar yValues = data[\"values\"];\r\n\t\t\t\tvar xValues = new Array();\r\n\t\t\t\t\r\n\t\t\t\t//construct X axis labels\r\n                var samplingPeriodSec = data[\"samplingPeriodSec\"];\r\n\t\t\t\tvar lastSampleSec = data[\"lastSampleSec\"];\r\n\t\t\t\tvar today = new Date();\r\n\t\t\t\ttoday.setSeconds(today.getSeconds() - lastSampleSec);\r\n\t\t\t\tfor (let i = 0; i < yValues.length; i++) {\r\n\t\t\t\t\ttoday.setSeconds(today.getSeconds() - samplingPeriodSec);\r\n\t\t\t\t\tconsole.log(today)\r\n\t\t\t\t\txValues.push(today.toLocaleTimeString(\'en-GB\'));\r\n\t\t\t\t}\r\n\t\t\t\tconsole.log(xValues)\r\n\t\t\t\tnew Chart(quantityName, {\r\n\t\t\t\t\ttype: \"line\",\r\n\t\t\t\t\tdata: {\r\n\t\t\t\t\t\tlabels: xValues,\r\n\t\t\t\t\t\tdatasets: [{\r\n\t\t\t\t\t\t\tlabel: quantityName,\r\n\t\t\t\t\t\t\tdata: data[\"values\"],\r\n\t\t\t\t\t\t\tborderColor: color,\r\n\t\t\t\t\t\t\tfill: false\r\n\t\t\t\t\t\t}]\r\n\t\t\t\t\t},\r\n\t\t\t\t\toptions: {\r\n\t\t\t\t\t\tlegend: {\r\n\t\t\t\t\t\t\tdisplay: true\r\n\t\t\t\t\t\t}\r\n\t\t\t\t\t}\r\n\t\t\t\t});\r\n\t\t\t}\r\n\t\t})\r\n\t}\r\n\t\r\n\t$(document).ready(function(){\r\n\t  $(\"button\").click(function(){\r\n          getData(\"red\", \"Temperature\", \"temperaturearray\")\r\n\t\t  getData(\"blue\", \"Humidity\", \"humidityarray\")\r\n\t\t  getData(\"green\", \"Pressure\", \"pressurearray\")\r\n\t  });\r\n\t});\r\n\t\r\n    function getDataPeriodic(){\r\n          getData(\"red\", \"Temperature\", \"temperaturearray\")\r\n\t\t  getData(\"blue\", \"Humidity\", \"humidityarray\")\r\n\t\t  getData(\"green\", \"Pressure\", \"pressurearray\")\r\n\t}\r\n\tgetDataPeriodic()\r\n\tsetInterval(getDataPeriodic, 30000);\r\n\r\n\t</script>\r\n</head>\r\n<body>\r\n\t<button>R</button>\r\n    <div class=\"content\">\r\n\t\t<canvas id=\"Temperature\" style=\"width:100%;max-width:600px\"></canvas>\r\n\t\t<canvas id=\"Humidity\" style=\"width:100%;max-width:600px\"></canvas>\r\n\t\t<canvas id=\"Pressure\" style=\"width:100%;max-width:600px\"></canvas>\r\n\t</div>\r\n</body>\r\n</html>";
   server.send(200, "text/html", html);
-}
-
-void handleUpload(){
-  server.send(200, "text/html", "upload");
 }
 
 void handleTemperature(){
@@ -213,12 +239,30 @@ void handlePressure(){
   server.send(200, "text/html", retBuff);
 }
 
+void handleTemperatureArray(){
+  char valuesBuffer[MEASURE_BUFFER_SIZE * 100];
+  valuesTemperature.bufferToJson(valuesBuffer);
+  arrayOfValuesToJsonResponseHelper(valuesBuffer);
+}
+
+void handleHumidityArray(){
+  char valuesBuffer[MEASURE_BUFFER_SIZE * 100];
+  valuesHumidity.bufferToJson(valuesBuffer);
+  arrayOfValuesToJsonResponseHelper(valuesBuffer);
+}
+
+void handlePressureArray(){
+  char valuesBuffer[MEASURE_BUFFER_SIZE * 100];
+  valuesPressure.bufferToJson(valuesBuffer);
+  arrayOfValuesToJsonResponseHelper(valuesBuffer);
+}
+
 void handleNotFound(){
   server.send(404, "text/plain", "Not found");
 }
 
 void printToLcd(){
-  char lcdData [SCREEN_NUMBER_OF_LINES][25]{0};
+  char lcdData [SCREEN_NUMBER_OF_LINES][SCREEN_NUMBER_OF_CHARS]{0};
   unsigned int ipBuffLen = WiFi.localIP().toString().length()+1;
   char ip[ipBuffLen]={0};
   WiFi.localIP().toString().toCharArray(ip, ipBuffLen);
@@ -229,31 +273,37 @@ void printToLcd(){
   drawText(lcdData);
 }
 
-//////////////// SETUP ///////////////////
+//////////////// SETUP /////////////////////////////////////////////////////
 void setup() {
   Serial.begin(9600);
   initLcd();
   initSensors();
   initWifi();
   initServer();
-  initBuffers();
+  valuesTemperature.clear();
+  valuesPressure.clear();
+  valuesHumidity.clear();
 }
-//////////////// EXECUTION LOOP ///////////////////
+//////////////// EXECUTION LOOP ///////////////////////////////////////////
 void loop() {
   server.handleClient();
 
+  // Call functions after SAMPLING_PERIOD_SEC
+  if (millis() - milisecondsFromLastMeasurement >= /*SAMPLING_PERIOD_SEC **/ 1000) {
+    milisecondsFromLastMeasurement = millis();
+    valuesTemperature.write(getTemperature());
+    valuesHumidity.write(getHumidity());
+    valuesPressure.write(getPressure());
+    //valuesTemperature.printVals();
+    //char buffer[MEASURE_BUFFER_SIZE * 100];
+    //valuesTemperature.bufferToJson(buffer);
+    //Serial.println(buffer);
+  }
 
-  // Call functions after certain amount of time
-  static int previousMiliseconds = millis();
-  if (millis() - previousMiliseconds >= 5000) {
-    previousMiliseconds = millis();
+  // Call functions after 5 seconds
+  static int displayRefreshTimer = millis();
+  if (millis() - displayRefreshTimer >= 5000) {
+    displayRefreshTimer = millis();
     printToLcd();
-    bufferWrite(valuesTemperature, valuesTemperaturePtr, getTemperature());
-    bufferWrite(valuesHumidity, valuesHumidityPtr, getTemperature());
-    bufferWrite(valuesPressure, valuesPressurePtr, getTemperature());
-    Serial.println("xxxxxxxxxxxxxxxxxxxxx");
-    char buffer[4000];
-    bufferToJson(valuesTemperature, valuesTemperaturePtr, buffer, 4000);
-    Serial.println(buffer);
   }
 }
